cb0421bdf9905bd3caeb6a12ee0b6d2daca29639
PIX <safonovmaksim0@mail.ru>
2023-07-06 10:26:55 +0900

    initial commit

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,26 @@
+# def strcount(string): # сложность  O(n ** 2)
+#     for symbol in string:
+#         counter = 0
+#         for sub_symbol in string:
+#             if symbol == sub_symbol:
+#                 counter += 1
+#         print(symbol, counter)
+
+# strcount('abcdceeea')
+
+# def strcount(string): # сложность O(n * m)
+#     for symbol in sorted(set(string)):
+#         counter = 0
+#         for sub_symbol in string:
+#             if symbol == sub_symbol:
+#                 counter += 1
+#         print(symbol, counter)
+
+# strcount('abcdceeea')
+
+def strcount(string): # сложность O(n * m)
+    syms_counter = {}
+    for symbol in string:
+        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+    print(syms_counter)
+strcount('aaabbcdeee')
